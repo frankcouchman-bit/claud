4775ff262f7d74c60af872d29d4c4c8c980954b4
frankcouchman-bit <frankcouchman@gmail.com>
2025-10-21 05:32:54 +0200

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,51 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Toaster } from 'react-hot-toast'
+import { AuthProvider } from './lib/auth'
+
+// Pages
+import Home from './pages/Home'
+import AiWriter from './pages/AiWriter'
+import WritingTool from './pages/WritingTool'
+import Login from './pages/Login'
+import Signup from './pages/Signup'
+import Dashboard from './pages/Dashboard'
+import Articles from './pages/Articles'
+import Tools from './pages/Tools'
+import Settings from './pages/Settings'
+import PrivacyPolicy from './pages/PrivacyPolicy'
+import TermsOfService from './pages/TermsOfService'
+import NotFound from './pages/NotFound'
+
+// Layout
+import DashboardLayout from './components/layout/DashboardLayout'
+
+function App() {
+  return (
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          {/* Public Routes */}
+          <Route path="/" element={<Home />} />
+          <Route path="/ai-writer" element={<AiWriter />} />
+          <Route path="/writing-tool" element={<WritingTool />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+          <Route path="/terms" element={<TermsOfService />} />
+          
+          {/* Protected Routes */}
+          <Route path="/dashboard" element={<DashboardLayout><Dashboard /></DashboardLayout>} />
+          <Route path="/articles" element={<DashboardLayout><Articles /></DashboardLayout>} />
+          <Route path="/tools" element={<DashboardLayout><Tools /></DashboardLayout>} />
+          <Route path="/settings" element={<DashboardLayout><Settings /></DashboardLayout>} />
+          
+          {/* 404 */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+        <Toaster position="top-right" />
+      </BrowserRouter>
+    </AuthProvider>
+  )
+}
+
+export default App
